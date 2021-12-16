34e766d19038522336e5bc301ce2eaed73bb7721
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-15 22:21:38 -0500

    installed react-router and chakra icons/ added navigation components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,93 @@
-const Navigation = (props) => {
+import { 
+  Box,
+  Container,
+  useColorModeValue,
+  Flex,
+  Heading, 
+  Stack,
+  Menu,
+  MenuButton,
+  MenuList,
+  IconButton
+} from '@chakra-ui/react'
+
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const LinkItem = ({ href, path, _target, children, ...props }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-    <main className="nav">
-      <img src={props.pic}></img>
-      <section><span>Dikshant Konwar Prasai</span></section>
-      <section><span>Web Developer</span></section>
-      <section>dikshantkunwar@gmail.com</section>
-    </main>
+    <Link
+      p={2}
+      bg={active ? 'grassTeal' : undefined}
+      color={active ? '#202023' : inactiveColor}
+      _target={_target}
+      >
+        {children}
+      </Link>
+  )
+}
+
+const Navigation = props => {
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      css = {{ backdropFilter: 'blur(10px)'}}
+      zIndex={1}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+        >
+          <Flex align="center" mr={5}>
+            <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+              Dikshant Konwar
+            </Heading>
+          </Flex>
+
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            display={{ base: 'none', md: 'flex' }}
+            width={{ base: 'full', md: 'auto'}}
+            alignItems="center"
+            flexGrow={1}
+            mt={{ base: 4, md: 0}}
+          >
+
+            <Box>Projects</Box>
+            <Box>Resume</Box>
+          
+          </Stack>
+
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu isLazy id="navbar-menu">
+              <MenuButton
+              as={IconButton}
+              icon={ <HamburgerIcon />}
+              variant="outline"
+              aria-label="Options"
+              />
+              <MenuList>
+                {/* Add menu links here ... */}
+              </MenuList>
+            </Menu>
+          </Box>
+        </Container>
+    </Box>
   )
 };
 
