a5347f2200f639fd98a04fe9418e1089f40ffafa
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-15 22:45:24 -0500

    added useColorMode to toggle dark mode

--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ import Navigation from './components/Navigation';
 import Skills from './components/Skills'
 import Footer from './components/Footer';
 
+import { Container, Flex, Button, useColorMode } from '@chakra-ui/react'
+
+
 function App() {
+  const { toggleColorMode } = useColorMode();
   return (
     <div className="App">
       <Navigation pic={profilePic} />
-      <section> 
+
+      <Container maxWidth="container.sm" padding={0} bg="grey.50">
+        
+        <Flex h="100vh" py={20} flexDirection="column">
+          ABC
+          <Button onClick={toggleColorMode}> Toggle dark theme </Button>
+        </Flex>
+      </Container>
+      {/* <section> 
         <Skills />
         <Education />
         <Experience />
-      </section>
+      </section> */}
       <Footer />
     </div>
   );
