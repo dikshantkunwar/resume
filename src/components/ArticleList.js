32a59626c5ddab77d7c490e1cf2ebedfb3b394a9
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-16 00:32:52 -0500

    added article list component and basic skeleton, installed eslint

---
+++ src/components/ArticleList.js
@@ -0,0 +1,42 @@
+import { 
+  Box,
+  Container, 
+  Heading } from "@chakra-ui/react"
+
+const ArticleList = () => {
+  return (
+    <Container maxW={'7xl'} p="12">
+      <Heading as="h1">Stories</Heading>
+      <Box
+        marginTop={{ base: '1', sm: '5' }}
+        display="flex"
+        flexDirection={{ base: 'column', sm: 'row' }}
+        justifyContent="space-between">
+              <Box
+                display="flex"
+                flex="1"
+                marginRight="3"
+                position="relative"
+                alignItems="center">
+                  <Box
+                width={{ base: '100%', sm: '85%' }}
+                zIndex="2"
+                marginLeft={{ base: '0', sm: '5%' }}
+                marginTop="5%">
+                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                  <Image
+                    borderRadius="lg"
+                    src={
+                      'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                    }
+                    alt="some good alt text"
+                    objectFit="contain"
+                  />
+                </Link>
+              </Box>
+
+            </Box>
+        </Box>
+    </Container>
+  )
+}
