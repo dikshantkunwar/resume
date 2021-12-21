5a2971d2a85f8a74ba5f757e2a24476d073a47ca
dikshantkunwar <dikshantkunwar@gmail.com>
2021-12-20 20:02:10 -0500

    initial flex box and project lsting in projectlist component

---
+++ src/components/sections/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Project from './Project'
+
+export default function ProjectList() {
+  return (
+
+    <Flex 
+      justifyContent="center">
+      <Project 
+        title="Lotide"
+        description="Lotide - clone of lodash"
+        date = "10/10/2021"
+        gitlink = "github link here/dikshantkunwar"
+        livepage = ""
+        image = ""
+        />
+        <Project 
+        title="Tweeter"
+        description="Tweeter - clone of twitter"
+        date = "10/10/2021"
+        gitlink = "github link here/dikshantkunwar"
+        livepage = ""
+        image = ""
+        />
+        <Project 
+        title="Scheduler"
+        description="Schedule appointments"
+        date = "10/10/2021"
+        gitlink = "github link here/dikshantkunwar"
+        livepage = ""
+        image = ""
+        />
+    </Flex>
+  )
+}
